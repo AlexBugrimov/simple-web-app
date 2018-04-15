2871de2114669a42f9950b7a413869fe885e2b1f
Alex Bugrimov <a.bugrimov@inbox.ru>
2018-04-16 00:14:25 +0300

    Add logger

--- src/main/java/net/ensode/glassfishbook/simpleapp/SimpleServlet.java
+++ src/main/java/net/ensode/glassfishbook/simpleapp/SimpleServlet.java
@@ -1,5 +1,7 @@
 package net.ensode.glassfishbook.simpleapp;
 
+import org.apache.log4j.Logger;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.http.HttpServlet;
@@ -7,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class SimpleServlet extends HttpServlet {
+    private final static Logger LOGGER = Logger.getLogger(SimpleServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
@@ -16,8 +19,8 @@ public class SimpleServlet extends HttpServlet {
             printWriter.println("<h2>");
             printWriter.println("If you are reading this, your application server is good to go!!!");
             printWriter.println("</h2>");
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
+        } catch (IOException ex) {
+            LOGGER.error("Ошибка работы сервлета", ex);
         }
     }
 }
